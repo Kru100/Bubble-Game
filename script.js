00081e11303b405c39c6f22d400dfafbbff599d6
Krunal <@kp1584085@gmail.com>
2023-09-02 12:13:03 +0530

    The Game if Done

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+function scoreCount(){
+    score += 10;
+    document.querySelector("#score").textContent = score;
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitVal").textContent = hitrn;
+}
+
 function makeBubble(){
     var clutter = "";
 
@@ -9,10 +23,33 @@ for(var i=1; i<=207; i++){
 document.querySelector("#pbtm").innerHTML = clutter;
 }
 
-var timer = 60;
 function runTimer(){
-    setInterval(() =>{
-       
+    var tstop = setInterval(() =>{
+       if(timer > 0)
+       {
+            timer--;
+            document.querySelector("#timerUI").textContent = timer;
+       }
+       else{
+        clearInterval(tstop);
+        document.querySelector("#pbtm").innerHTML = `<h1>Time Up!!!!</h1> <br> <h1>Your Score : ${hitrn}</h1>`
+        //document.querySelector("#pbtm").innerHTML = ``
+       }
     }, 1000)
 }
+
+document.querySelector("#pbtm").addEventListener("click",(e) => {
+    let val = Number(e.target.textContent);
+    if(val === hitrn){
+        scoreCount();
+        makeBubble();
+        getNewHit();
+    }
+    else{
+        makeBubble();
+        getNewHit();
+    }
+})
+getNewHit()
+runTimer();
 makeBubble();
